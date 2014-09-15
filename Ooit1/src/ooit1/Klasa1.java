2089dad7c6b470546d9155b9f665791fdb2c28e5
djordjeprzulj <przulj@gmail.com>
2014-09-15 14:42:37 +0200

    sabiranje dvije hardcoded promenljive

--- Ooit1/src/ooit1/Klasa1.java
+++ Ooit1/src/ooit1/Klasa1.java
@@ -3,8 +3,10 @@ package ooit1;
 public class Klasa1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int prviBroj = 2;
+		int drugiBroj = 6;
+		int zbir = prviBroj + drugiBroj;
+		System.out.println(zbir);
 	}
 
 }

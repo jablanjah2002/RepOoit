cf3ba1547ee9e9790c3ed078e680f77327aaaa34
djordjeprzulj <przulj@gmail.com>
2014-10-13 14:47:17 +0200

    Jos jedna klasa

---
+++ Ooit1/src/rekapitulacija/Promenljive.java
@@ -0,0 +1,12 @@
+package rekapitulacija;
+
+public class Promenljive {
+
+	public static void main(String[] args) {
+		//	1. deklarisati pomenljivu pod nazivom "broj" cije su vrednosti iz domena celih brojeva 
+		int i;
+		//	2. 
+
+	}
+
+}
